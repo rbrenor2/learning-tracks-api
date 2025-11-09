4ca5a161e6a9b9a05e8bbccd855c25c6fc77d446
Breno Rios <rbrenorios@gmail.com>
2025-11-09 21:29:42 +0000

    fix: adding NoSpecialChars validator to Track dto

--- src/modules/tracks/dto/create-track.dto.ts
+++ src/modules/tracks/dto/create-track.dto.ts
@@ -1,10 +1,12 @@
-import { IsOptional, IsArray, IsString, ArrayMinSize } from "class-validator";
-import { CustomErrorMessages } from "src/common/enums/custom-error-messages.enum";
+import { ArrayMinSize, IsArray, IsOptional, IsString } from "class-validator";
+import { CustomErrorMessages } from "../../../common/enums/custom-error-messages.enum";
+import { NoSpecialChars } from "../../../common/validators/no-special-chars.validator";
 
 export class CreateTrackDto {
     @IsOptional()
     @IsArray({ message: CustomErrorMessages.tracksIsArray })
     @IsString({ each: true, message: CustomErrorMessages.trackMustBeString })
     @ArrayMinSize(1, { message: CustomErrorMessages.atLeastOneTrack })
+    @NoSpecialChars({ message: CustomErrorMessages.unallowedChars })
     names: string[]
 }
